0d8fa913889c89e07875453fb942bd680045c9a8
Jessica Watts <jessperr182@hotmail.com>
2020-09-13 03:50:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(groceries)
+  groceries = {oranges: 2, apples: 4, plums: 8}
+  key_for_min_value(groceries)
 end
