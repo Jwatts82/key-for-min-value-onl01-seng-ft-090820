267739a8ad5c74969863c0f62ecc9a9ac20b8885
Jessica Watts <jessperr182@hotmail.com>
2020-09-13 04:01:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(groceries)
-  groceries = {oranges: 2, apples: 4, plums: 8}
-  groceries do |item, value|
-    if value == 
-  end
+def key_for_min_value(hash)
+  hash.key(hash.values.min)
 end
